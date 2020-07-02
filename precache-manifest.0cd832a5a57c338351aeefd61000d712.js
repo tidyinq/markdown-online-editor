22c26b8bf9ef2089870e45c69e4378e2bbc5e978
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2020-07-02 23:17:02 +0800

    🎉 update release

---
+++ precache-manifest.0cd832a5a57c338351aeefd61000d712.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "db483cb155e86c22152f",
+    "url": "/css/app.87739e18.css"
+  },
+  {
+    "revision": "db483cb155e86c22152f",
+    "url": "/js/app.45f971e5.js"
+  },
+  {
+    "revision": "d7a4ae59c00b7eae7011",
+    "url": "/css/chunk-00ee8072.0b88bc64.css"
+  },
+  {
+    "revision": "d7a4ae59c00b7eae7011",
+    "url": "/js/chunk-00ee8072.b6ca8e69.js"
+  },
+  {
+    "revision": "378afa1dbce80c839086",
+    "url": "/js/chunk-0b046b52.d45ef4ab.js"
+  },
+  {
+    "revision": "36890fe1777dbd2e35a6",
+    "url": "/css/chunk-22239e24.0b88bc64.css"
+  },
+  {
+    "revision": "36890fe1777dbd2e35a6",
+    "url": "/js/chunk-22239e24.1e01ad21.js"
+  },
+  {
+    "revision": "1f2688bd1684060d9ca3",
+    "url": "/js/chunk-2d216257.df162628.js"
+  },
+  {
+    "revision": "990f8cd42031bdf7a776",
+    "url": "/js/chunk-2d238433.73eedfbd.js"
+  },
+  {
+    "revision": "81f848fd25028b92401a",
+    "url": "/css/chunk-2e73aee3.93c3732a.css"
+  },
+  {
+    "revision": "81f848fd25028b92401a",
+    "url": "/js/chunk-2e73aee3.664b36aa.js"
+  },
+  {
+    "revision": "817ce977be6502ad6e18",
+    "url": "/css/chunk-34d2c1c4.a1e659d5.css"
+  },
+  {
+    "revision": "817ce977be6502ad6e18",
+    "url": "/js/chunk-34d2c1c4.b1233365.js"
+  },
+  {
+    "revision": "2e089ee021b71144ce31",
+    "url": "/css/chunk-5d3ae0d8.0b88bc64.css"
+  },
+  {
+    "revision": "2e089ee021b71144ce31",
+    "url": "/js/chunk-5d3ae0d8.0e5864ad.js"
+  },
+  {
+    "revision": "4858e9b24884bdbc95ee",
+    "url": "/js/chunk-a4189212.a6657841.js"
+  },
+  {
+    "revision": "41467617c9399c632add",
+    "url": "/css/chunk-a4b3ff6c.864c088c.css"
+  },
+  {
+    "revision": "41467617c9399c632add",
+    "url": "/js/chunk-a4b3ff6c.4ce45804.js"
+  },
+  {
+    "revision": "d472166369a24cef85c0",
+    "url": "/js/element.7a4800b8.js"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "cd382dc8a9d6317864b5810a320effc5",
+    "url": "/fonts/league-gothic.cd382dc8.woff"
+  },
+  {
+    "revision": "9900a4643cc63c5d8f969d2196f72572",
+    "url": "/fonts/league-gothic.9900a464.eot"
+  },
+  {
+    "revision": "91295fa87df918411b49b7531da5d558",
+    "url": "/fonts/league-gothic.91295fa8.ttf"
+  },
+  {
+    "revision": "e129e6c77cef1743ff19a9f0b2131cb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bf35c3805e83f0178f9e224d8d0febca",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "369f6129748a531567ab3535689be69a",
+    "url": "/site.webmanifest"
+  }
+];
